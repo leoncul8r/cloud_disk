eabca5ddac9bf6e2f00449d9096aed5e6814ca06
leoncul8r <953733547@qq.com>
2020-06-26 17:20:11 +0800

    Initial commit

---
+++ src/main/java/leonc/cloud_disk/CloudDiskApplication.java
@@ -0,0 +1,15 @@
+package leonc.cloud_disk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDiskApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (CloudDiskApplication.class, args);
+    }
+
+}
